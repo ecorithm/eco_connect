08f9bfba3fbfea7378a57e8cdc4b899c54eebbf8
Bradford Littooy <blittooy@ecorithm.com>
2018-01-22 15:35:03 -0800

    fix example usage

--- eco_connect/facts_service.py
+++ eco_connect/facts_service.py
@@ -224,6 +224,7 @@ class FactsService(BaseRequest):
 
 
     **Example Usage:**
+
     >>> from eco_connect import FactsService
     >>> facts_service = FactsService()
     >>> facts_service.get_facts(building_id=26,
