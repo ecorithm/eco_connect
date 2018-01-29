8b1955a9e8b3fa3660ec4fb3758d14c55f162028
Bradford Littooy <blittooy@ecorithm.com>
2018-01-29 15:59:31 -0800

    facts-avg update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_requirements():
 
 
 setup(
-    name='ecp-connect',
+    name='eco-connect',
     version='0.19.0',
     description='Ecorithm\'s private eco-connect ',
     long_description='Please see http://eco-connect.readthedocs.io/en/latest/ '
